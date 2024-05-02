b3ff3abcf61b301336611b777bee2cb6ed8cd992
Ulfflo <145294663+Ulfflo@users.noreply.github.com>
2024-05-02 11:42:33 +0200

    createaccount

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import cors from "cors";
+import mysql from "mysql2/promise";
+import bcrypt from "bcrypt";
+
 
 dotenv.config();
 
@@ -9,7 +13,51 @@ const app = express();
 const PORT = 3009;
 
 //middleware
+app.use(cors());
 app.use(bodyParser.json());
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "story_tailor",
+  port: 8889,
+});
+
+// help function to make code look nicer
+async function query(sql, params) {
+  const [results] = await pool.execute(sql, params);
+  return results;
+}
+
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Kryptera lösenordet innan det hamnar i DB
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  try {
+    // Skapa användaren i users-tabellen
+    const userResult = await query(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      [username, hashedPassword]
+    );
+    const userId = userResult.insertId; // Få det nya användar-ID:t
+
+    // Skapa ett konto för användaren i accounts-tabellen
+    await query(
+      "INSERT INTO accounts (user_id, balance) VALUES (?, ?)",
+      [userId, 0] // Skapa med 0 kr som saldo
+    );
+
+    res.status(201).send("User and account created");
+  } catch (error) {
+    console.error("Error creating user and account", error);
+    res.status(500).send("Error creating user and account");
+  }
+});
+
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>FairyTailor</h1>
       <p>This is a storytelling app</p>
-      <button>Create Account</button>
+      <button>
+        <Link href="/CreateAccount">Create Account</Link>
+      </button>
       <button>Log in</button>
       <button>Start a story</button>
     </main>
