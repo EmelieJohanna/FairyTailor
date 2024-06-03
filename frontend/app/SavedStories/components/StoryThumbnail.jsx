a339516ace0b9fd614354ba83bee5e554b7b9d61
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-06-03 15:48:18 +0200

    fixed DeleteStoryBtn

--- frontend/app/SavedStories/components/StoryThumbnail.jsx
+++ frontend/app/SavedStories/components/StoryThumbnail.jsx
@@ -10,25 +10,27 @@ const StoryThumbnail = ({
   isEditing,
 }) => {
   return (
-    <div className="story-thumbnail flex flex-col items-center">
-      <img
-        src={story.image_url}
-        alt={story.story_title}
-        className="w-full h-48 object-cover"
-      />
-      <div className="flex flex-col items-center mt-4">
-        <button
-          className="bg-blue-500 text-white py-2 px-4 rounded mb-2"
-          onClick={onLoadFromSavedPage}
-        >
-          Continue from where you left off
-        </button>
-        <button
-          className="bg-green-500 text-white py-2 px-4 rounded"
-          onClick={onLoadFromStart}
-        >
-          Start from the beginning
-        </button>
+    <div className="relative">
+      <div className="flex flex-col items-center ">
+        <img
+          src={story.image_url}
+          alt={story.story_title}
+          className="w-full h-48 object-cover"
+        />
+        <div className="flex flex-col items-center mt-4">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded mb-2"
+            onClick={onLoadFromSavedPage}
+          >
+            Continue from where you left off
+          </button>
+          <button
+            className="bg-green-500 text-white py-2 px-4 rounded"
+            onClick={onLoadFromStart}
+          >
+            Start from the beginning
+          </button>
+        </div>
       </div>
       {isEditing && (
         <div className="absolute -top-5 right-4">
